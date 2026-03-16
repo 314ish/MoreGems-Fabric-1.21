2d8f4f43c0f12c610078121c2a6fb027d82a9dc6
314ish <ruckusflashlight@gmail.com>
2026-03-15 21:03:43 -0700

    Added Sapphire Ore block.

--- src/main/java/com/blazingblaze8/moregems/block/ModBlocks.java
+++ src/main/java/com/blazingblaze8/moregems/block/ModBlocks.java
@@ -1,14 +1,20 @@
 package com.blazingblaze8.moregems.block;
 
 import com.blazingblaze8.moregems.MoreGems;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
+
+    public static final Block SAPPHIRE_ORE = registerBlock("sapphire_ore",
+            new Block(AbstractBlock.Settings.create().strength(3.0f).requiresTool()));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -22,5 +28,9 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         MoreGems.LOGGER.info("Registering mod blocks for" + MoreGems.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
+            entries.add(SAPPHIRE_ORE);
+        });
     }
 }
